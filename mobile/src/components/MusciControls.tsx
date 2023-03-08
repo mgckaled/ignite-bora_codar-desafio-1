a2963334e8ec5d53b5006f04c48575b7ceb0882e
mgckaled <marcelgckaled@gmail.com>
2023-03-08 18:18:19 -0300

    feat(mobile): install expo-av and config sound

--- mobile/src/components/MusciControls.tsx
+++ mobile/src/components/MusciControls.tsx
@@ -1,18 +1,50 @@
+import { Audio } from "expo-av"
 import { HStack, useTheme } from "native-base"
-import { FastForward, Play, Rewind } from "phosphor-react-native"
+import { FastForward, Pause, Play, Rewind } from "phosphor-react-native"
+import React, { useEffect, useState } from "react"
 import { TouchableOpacity } from "react-native"
 
 export function MusicControl() {
   const { colors } = useTheme()
+  const [sound, setSound] = useState<Audio.Sound | null>(null)
+
+  async function playSound() {
+    console.log("Loading sound")
+    const { sound } = await Audio.Sound.createAsync(require("../assets/audio/audio.mp3"))
+    setSound(sound)
+
+    console.log("Playing Sound")
+    await sound.playAsync()
+  }
+
+  function stopSound() {
+    setSound(null)
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound")
+          sound.unloadAsync()
+        }
+      : undefined
+  }, [sound])
 
   return (
     <HStack p={6} mt={8} alignContent='center' justifyContent='space-between'>
       <TouchableOpacity>
         <Rewind size={30} weight='fill' color={colors.trackColor} />
       </TouchableOpacity>
-      <TouchableOpacity>
-        <Play size={30} weight='fill' color={colors.trackColor} />
-      </TouchableOpacity>
+      {sound === null ? (
+        <TouchableOpacity onPress={playSound}>
+          <Play size={30} weight='fill' color={colors.trackColor} />
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity onPress={stopSound}>
+          <Pause size={30} weight='fill' color={colors.trackColor} />
+        </TouchableOpacity>
+      )}
+
       <TouchableOpacity>
         <FastForward size={30} weight='fill' color={colors.trackColor} />
       </TouchableOpacity>
